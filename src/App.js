48c7bf1a8b7c44a5f27b402335a83563903041c4
rickyjulpiter <rickyjulpiter14@gmail.com>
2021-01-28 15:48:58 +0700

    fix CORS issue

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
     }
 
     componentDidMount() {
-        fetch("http://jsonplaceholder.typicode.com/users")
+        fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
             .then((users) => this.setState({ monsters: users }));
     }
